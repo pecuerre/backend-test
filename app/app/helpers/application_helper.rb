b29db8153548480ac6f08b17373d6023a20591cf
Pedro Quintero <pecuerre@gmail.com>
2016-07-15 19:20:10 -0400

    showing images lined up

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
 
+	def medium_image_tag(string)
+		generic_image_tag(string, "350x250")
+	end
+
 	def small_image_tag(string)
 		generic_image_tag(string, "64x64")
 	end
 
 	def generic_image_tag(string, size)
 		if string.present?
-			string
+			image_tag string, :size => size
 		else
 			holder_tag size, '', 'gray', :class => 'pull-left highlight-image'
 		end
