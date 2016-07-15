2831fdd41917bc29ffc14392017b3e36b49c8461
Pedro Quintero <pecuerre@gmail.com>
2016-07-15 17:36:49 -0400

    adding event dates

--- app/app/models/event.rb
+++ app/app/models/event.rb
@@ -1,8 +1,23 @@
 class Event < ActiveRecord::Base
 	SEPARATOR = "||"
+	has_many :event_dates
+	after_save :update_event_dates
+	before_destroy :remove_event_dates
 
+	def date_list
+		self.dates.to_s.split(Event::SEPARATOR)
+	end
 
-  def date_list
-  	self.dates.to_s.split(Event::SEPARATOR)
-  end
+	def update_event_dates
+		self.remove_event_dates
+		self.date_list.each do |d|
+			EventDate.create(:when => d, :event_id => self.id)
+		end
+	end
+
+	def remove_event_dates
+		self.event_dates.each do |ed|
+			ed.destroy
+		end
+	end
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715172928) do
+ActiveRecord::Schema.define(version: 20160715211000) do
+
+  create_table "event_dates", force: :cascade do |t|
+    t.datetime "when"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
