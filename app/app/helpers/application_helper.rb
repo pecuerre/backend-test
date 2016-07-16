d6734ae64b4dd2e9b5c8912c6f9cf8e2bdeb9920
Pedro Quintero <pecuerre@gmail.com>
2016-07-15 20:39:38 -0400

    vista detalles

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -1,18 +1,20 @@
 module ApplicationHelper
 
 	def medium_image_tag(string)
-		generic_image_tag(string, "350x350")
+		generic_image_tag(string, "350x350", :pull_left => false)
 	end
 
 	def small_image_tag(string)
 		generic_image_tag(string, "64x64")
 	end
 
-	def generic_image_tag(string, size)
+	def generic_image_tag(string, size, options = {:pull_left => true})
+		classes = ['highlight-image']
+		classes << 'pull-left' if options[:pull_left]
 		if string.present?
-			image_tag string, :size => size, :class => 'pull-left highlight-image'
+			image_tag string, :size => size, :class => classes
 		else
-			holder_tag size, '', 'gray', :class => 'pull-left highlight-image'
+			holder_tag size, '', 'gray', :class => classes
 		end
 	end
 end
